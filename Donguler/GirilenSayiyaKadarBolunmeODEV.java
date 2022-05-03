2a964d36dbd49e4f96c02d615d31ad82a4beb704
Muhammed Yıldırım <muhammedyildirim48@gmail.com>
2022-05-03 22:52:35 +0300

    For Döngüsü Örnekleri

---
+++ Donguler/GirilenSayiyaKadarBolunmeODEV.java
@@ -0,0 +1,19 @@
+package Donguler;
+import java.util.Scanner;
+
+public class GirilenSayiyaKadarBolunmeODEV {
+    public static void main(String[] args) {
+        int sayi1;
+
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("Sayı Giriniz : ");
+        sayi1=input.nextInt();
+
+        for (int i=1; i<=sayi1; i++){
+            if (i % 3 == 0 && i % 4 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ Donguler/GirilenSayiyaKadarCiftSayilarıBul.java
@@ -0,0 +1,17 @@
+package Donguler;
+import java.util.Scanner;
+public class GirilenSayiyaKadarCiftSayilarıBul {
+    public static void main(String[] args) {
+        int k ;
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("Sayı Giriniz : ");
+        k=input.nextInt();
+
+        for (int i = 1; i <= k; i++){
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
