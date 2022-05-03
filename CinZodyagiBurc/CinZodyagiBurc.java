36294705b5ca591eccdd6f7de19eb6ef882ccd65
Muhammed Yıldırım <muhammedyildirim48@gmail.com>
2022-05-03 21:10:31 +0300

    Çin Zodyağı Ödevi Eklendi

---
+++ CinZodyagiBurc/CinZodyagiBurc.java
@@ -0,0 +1,53 @@
+package CinZodyagiBurc;
+import java.util.Scanner;
+public class CinZodyagiBurc {
+    public static void main(String[] args) {
+        int year;
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("Doğum Yılınızı Giriniz : ");
+        year=input.nextInt();
+
+        int mod;
+        mod=year%12;
+
+        switch (mod){
+            case 0:
+                System.out.print("Maymun");
+            break;
+            case 1:
+                System.out.print("Horoz");
+            break;
+            case 2:
+                System.out.print("Köpek");
+                break;
+            case 3:
+                System.out.print("Domuz");
+                break;
+            case 4:
+                System.out.print("Fare");
+                break;
+            case 5:
+                System.out.print("Öküz");
+                break;
+            case 6:
+                System.out.print("Kaplan");
+                break;
+            case 7:
+                System.out.print("Tavşan");
+                break;
+            case 8:
+                System.out.print("Ejderha");
+                break;
+            case 9:
+                System.out.print("Yılan");
+                break;
+            case 10:
+                System.out.print("At");
+                break;
+            case 11:
+                System.out.print("Koyun");
+                break;
+        }
+    }
+}
